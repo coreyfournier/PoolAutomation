61438f8478e02caaae2d65045f858cdc38757731
corey.fournier@gmail.com <corey.fournier@gmail.com>
2022-05-02 16:23:54 -0500

    Added a simple powershell deployment script. Will require the password and user +server

---
+++ DeployCode.ps1
@@ -0,0 +1,20 @@
+###########################################################################################
+# Very Very simple deployment script using putty secure copy 
+#
+#
+###########################################################################################
+Param(	
+	[string]$password,
+    #Format is {user account}@{Ip or server name}
+    [string]$computer,
+    [string]$targetPath = "/home/pi/pool/"
+)
+
+#Stop execution on the first error
+$ErrorActionPreference = "Stop"
+
+#Copy the files specified from the current directory
+Invoke-Expression -Command "pscp -r -pw $($password) *.py  $($computer):$($targetPath)"
+Invoke-Expression -Command "pscp -r -pw $($password) *.txt  $($computer):$($targetPath)"
+Invoke-Expression -Command "pscp -r -pw $($password) *.html $($computer):$($targetPath)"
+Invoke-Expression -Command "pscp -r -pw $($password) lib\*.py  $($computer):$($targetPath)lib"
