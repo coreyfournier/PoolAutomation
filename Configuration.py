9211d866392e8801f6852bf13d7397839c409d05
corey.fournier@gmail.com <corey.fournier@gmail.com>
2023-03-17 16:47:34 -0500

    Made the spacing consistent.

--- Configuration.py
+++ Configuration.py
@@ -137,18 +137,18 @@ def allChangeNotification(event:Event):
         if(DependencyContainer.stateLogger != None):
             #Using the nameing convention where the Id number matches the number of the column. Temp sensor 1 matches temperature1
             DependencyContainer.stateLogger.add(
-                temperature1= DependencyContainer.temperatureDevices.getById(1).getLast(),
-                temperature2= DependencyContainer.temperatureDevices.getById(2).getLast(),
-                temperature3= DependencyContainer.temperatureDevices.getById(3).getLast(),
-                temperature4= DependencyContainer.temperatureDevices.getById(4).getLast(),
-                pumpState1=DependencyContainer.pumps.getById(1).currentSpeed.name,
-                valveState1=DependencyContainer.valves.getById(1).isOn,
-                valveState2=DependencyContainer.valves.getById(2).isOn,
-                ScheduleActive1=DependencyContainer.schedules.getById(1).isRunning,
-                ActionActive1= DependencyContainer.actions.get()[0].overrideSchedule,
-                ActionActive2= DependencyContainer.actions.get()[1].overrideSchedule,
-                ActionActive3= DependencyContainer.actions.get()[2].overrideSchedule,
-                ActionActive4= DependencyContainer.actions.get()[3].overrideSchedule
+                temperature1 = DependencyContainer.temperatureDevices.getById(1).getLast(),
+                temperature2 = DependencyContainer.temperatureDevices.getById(2).getLast(),
+                temperature3 = DependencyContainer.temperatureDevices.getById(3).getLast(),
+                temperature4 = DependencyContainer.temperatureDevices.getById(4).getLast(),
+                pumpState1 = DependencyContainer.pumps.getById(1).currentSpeed.name,
+                valveState1 = DependencyContainer.valves.getById(1).isOn,
+                valveState2 = DependencyContainer.valves.getById(2).isOn,
+                ScheduleActive1 = DependencyContainer.schedules.getById(1).isRunning,
+                ActionActive1 = DependencyContainer.actions.get()[0].overrideSchedule,
+                ActionActive2 = DependencyContainer.actions.get()[1].overrideSchedule,
+                ActionActive3 = DependencyContainer.actions.get()[2].overrideSchedule,
+                ActionActive4 = DependencyContainer.actions.get()[3].overrideSchedule
             )
         
         displayRotation = 1 + displayRotation
