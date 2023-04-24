44c15837378c46e29caeb7c592e3136499d9882f
corey.fournier@gmail.com <corey.fournier@gmail.com>
2023-04-23 20:49:29 -0500

    Working on the styles

--- www/src/app/app.component.ts
+++ www/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '../../css/bootstrap.min.css',
+    '../../css/site.css',
+    '../../css/bootstrap-toggle.min.css'
+  ]
 })
-
+/*
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/site.css">
+    <link rel="stylesheet" href="/css/bootstrap-toggle.min.css">
+    <link rel="stylesheet" href="/apexcharts/apexcharts.css">
+*/
 export class AppComponent {
   title = 'Pool Automation';
 }
