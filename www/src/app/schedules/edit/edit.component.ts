51040aec5bbaa63549496ee51ec687ceb6442748
corey.fournier@gmail.com <corey.fournier@gmail.com>
2023-09-07 20:12:03 -0500

    troubleshooting the error. Phoning a friend

--- www/src/app/schedules/edit/edit.component.ts
+++ www/src/app/schedules/edit/edit.component.ts
@@ -5,6 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ScheduleInfo } from '../schedule';
 import { DatePipe } from '@angular/common';
 import { EventInfo, ScheduleChangeEvent } from '../../app.events';
+import { FormBuilder, FormGroup, Validators,FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-schedules-edit',
@@ -13,21 +14,30 @@ import { EventInfo, ScheduleChangeEvent } from '../../app.events';
 })
 
 export class ScheduleEditComponent {
-  
+  form = this.formBuilder.group({
+    //... other form controls ...
+    lessons: this.formBuilder.array([])
+});
+
   scheduleInfo:ScheduleInfo = {
     schedules:[],
     overrides:[]
   };
 
+  get lessons():FormArray {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
   private scheduleUrl = environment.apiUrl + 'schedule/schedules';  // URL to web api
   datepipe: DatePipe = new DatePipe('en-US');
   timeFormat:string = 'hh:mm a';
 
-  constructor(private http: HttpClient, public zone: NgZone) { 
+  constructor(private http: HttpClient, public zone: NgZone, private formBuilder: FormBuilder) { 
 
   }     
 
   ngOnInit(): void {
+    
     this.getSchedules().subscribe(s=> this.scheduleInfo = s);
    
   }
@@ -35,4 +45,21 @@ export class ScheduleEditComponent {
   getSchedules():Observable<ScheduleInfo>{
     return this.http.get<ScheduleInfo>(this.scheduleUrl);
   }
+
+  onSubmit() {    
+    console.log("Submitted form");
+  }
+
+  addLesson():void  {
+    const lessonForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      level: ['beginner', Validators.required]
+    });
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number):void {
+    this.lessons.removeAt(lessonIndex);
+  }
+
 }
